089daead538315281f45778e69fb5d3c5332a53e
Mikel <mikelromero11@gmail.com>
2025-01-29 03:22:13 +0100

    add email vaildation

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 # Modelos base para evitar redundancia
 class ClienteBase(BaseModel):
     nombre: str
     dni: str
-    email: str
+    email: EmailStr
     capital_solicitado: float
 
 class SimulacionHipotecaBase(BaseModel):
